2679a92b73786b3634fb9c6f82efec6657fc9310
ChhitizV <chhitizvaidya10@gmail.com>
2024-09-08 19:05:21 +0545

    Question 1

--- OOP/exercise1.py
+++ OOP/exercise1.py
@@ -1,1 +1,11 @@
 # Create a class Person with attributes name and age. Include a method greet() that prints "Hello, my name is name and I am age years old". Create an instance of Person and call the greet() method.
+
+class Person: 
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+
+        
