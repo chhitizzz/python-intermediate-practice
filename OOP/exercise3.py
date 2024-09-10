4071d70345ad3fe40d87c573c1a3ca6e912f96a4
ChhitizV <chhitizvaidya10@gmail.com>
2024-09-10 21:10:09 +0545

    exercise 3

--- OOP/exercise3.py
+++ OOP/exercise3.py
@@ -20,3 +20,20 @@ class Library:
             print(f"'{book}' is in the library.")
         else:
             print(f"'{book}' is not available in the library.")
+
+    def display_books(self):
+        if self.books:
+            print("The books available in the library are:")
+            for book in self.books:
+                print(f"- {book}")
+        else:
+            print("No books are available in the library.")
+
+new_book = Library()
+new_book.add_books("The Alchemist")
+new_book.add_books("1984")
+new_book.add_books("Rich Dad, Poor Dad")
+new_book.add_books("Can't Hurt Me")
+new_book.add_books("Becoming")
+
+new_book.display_books()
