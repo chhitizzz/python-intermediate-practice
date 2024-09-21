ac852e533de66cc5a0e96a21f49d13dc0c3cb152
ChhitizV <chhitizvaidya10@gmail.com>
2024-09-21 21:31:55 +0545

    Question 4

---
+++ OOP/exercise4.py
@@ -0,0 +1,1 @@
+# Create a base class Animal with attributes name and species, and a method make_sound(). Then create a subclass Dog that inherits from Animal and adds a method fetch(). Instantiate a Dog object and call both methods.
