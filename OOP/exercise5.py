7f0b421fcce668508f137aab705d24502c6fb495
ChhitizV <chhitizvaidya10@gmail.com>
2024-09-22 12:49:34 +0545

    Question 5

--- OOP/exercise5.py
+++ OOP/exercise5.py
@@ -6,4 +6,22 @@ class Person:
         self.last_name = last_name
     
     def get_full_name(self) -> None:
-        print(f"My name is {self.first_name} {self.last_name}")
+        return f"{self.first_name} {self.last_name}"
+
+class Employee(Person):
+    def __init__(self, first_name, last_name, employee_id: int, position: str) -> None:
+        super().__init__(first_name, last_name)
+        self.employee_id = employee_id
+        self.position = position
+
+    def get_employee_details(self) -> None:
+        print(f"My name is {self.get_full_name()}. I am a {self.position} and my employee ID is {self.employee_id}.")
+
+Senna: Employee = Employee("Ayrton", "Senna", 1, "CEO")
+Senna.get_employee_details()
+
+Maxwell: Employee = Employee("Glenn", "Maxwell", 2, "Software Developer")
+Maxwell.get_employee_details()
+
+George: Employee = Employee("George", "Best", 3, "Automation Engineer")
+George.get_employee_details()
